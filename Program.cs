e2b2c198f1f7644e4ea156240812e604bc6ce805
prasadacharya <prasada@mail.usf.edu>
2020-01-22 22:18:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Exercise 4
+// Input a value (n) from the user and calculate the sum of first n natural numbers.
+// Eg. n=5. sum = 1+2+3+4+5 = 15
+
+using System;
+
+namespace Sum_of_n_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide your input number");
+            string N = Console.ReadLine();
+            int n = Convert.ToInt32(N);
+
+            int sum = 0;
+
+            for (int i =1; i<=n; i++)
+
+            {
+                 sum = sum + i;
+            }
+
+            Console.WriteLine("Sum of first n natural numbers is"+sum);
+
+        }
+    }
+}
